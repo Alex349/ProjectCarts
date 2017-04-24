65542424461a702ea0b55ae2a29bee90722c01c8
Alex349 <alex349v@gmail.com>
2017-04-24 16:37:53 +0200

    Revert "Commit for revert"
    This reverts commit 25d251f0c7c67ba4a2be2c7be1cea1a1c46c2980.

--- Assets/Scripts/CheckPoints.cs
+++ Assets/Scripts/CheckPoints.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class CheckPoints : MonoBehaviour {
     private Transform playerTransform;
-    private CarCheckPoints carCheckPoints;
+    public CarCheckPoints carCheckPoints;
     // Use this for initialization
     void Start ()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Kart").transform;
-        carCheckPoints = GameObject.FindGameObjectWithTag("Kart").GetComponent<CarCheckPoints>();
+
     }
 
-    void OnTriggerEnter(Collider col)
+    void OnTriggerEnter(Collider other)
     {
         //Is it the Player who enters the collider?
-        if (!col.CompareTag("Kart"))
+        if (!other.CompareTag("Kart"))
             return; //If it's not the player dont continue
 
         //Is this transform equal to the transform of checkpointArrays[currentCheckpoint]?
-        if (this.transform == playerTransform.GetComponent<CarCheckPoints>().checkPointArray[carCheckPoints.currentCheckpoint].transform)
+        if (transform == playerTransform.GetComponent<CarCheckPoints>().checkPointArray[carCheckPoints.currentCheckpoint].transform)
         {
             //Check so we dont exceed our checkpoint quantity
             if (carCheckPoints.currentCheckpoint + 1 < playerTransform.GetComponent<CarCheckPoints>().checkPointArray.Length)

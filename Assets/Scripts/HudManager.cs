783469c383797b420a4f078c0d33c39e30cda29c
Alex349 <alex349v@gmail.com>
2017-04-26 03:30:15 +0200

    banana :banana:

--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -1,41 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HudManager : MonoBehaviour
 {
-    
     public CarCheckPoints carCheckPoints;
     public Image itemImage;
     public IA_Item ia_Item;
     public Sprite[] itemSpriteList;
-    public Text currentPosition_Text, time_Text, currentLap_Text, totalLaps_Text;
-    public float currentPosition, time, secondsCount, minuteCount, milisecondsCount, currentLap, totalLaps;
-
-    // Use this for initialization
-    void Start()
-    {
-    }
+    public Text currentPosition_Text, time_Text, currentLap_Text, totalLaps_Text, coins_Text;
+    private float currentPosition, time, secondsCount, minuteCount, milisecondsCount, currentLap, totalLaps;
 
     // Update is called once per frame
     void Update()
     {
         UpdateTimerUI();
         UpdateItemUI();
+        //Laps UI
         currentPosition_Text.text = currentPosition.ToString();
         currentLap = carCheckPoints.currentLap ;
         currentLap_Text.text = currentLap.ToString();
         totalLaps_Text.text = totalLaps.ToString();
+        //Coins UI
+        coins_Text.text = "Coins:" + ia_Item.money.ToString();
     }
 
     public void UpdateTimerUI()
     {
         //set timer UI 3 digits als milisegons
-        milisecondsCount += Time.deltaTime * 100;
-        time_Text.text = minuteCount + ": " + (int)secondsCount + ", " + (float)milisecondsCount;
+        milisecondsCount += Time.deltaTime * 1000;
 
-        if (milisecondsCount >= 100)
+        time_Text.text = minuteCount + ": " + (int)secondsCount + ", " + milisecondsCount.ToString("000").Truncate(3);
+
+        if (milisecondsCount >= 999)
         {
             secondsCount++;
             milisecondsCount = 0;
@@ -65,5 +64,14 @@ public class HudManager : MonoBehaviour
         {
             itemImage.sprite = itemSpriteList[3];
         }
+    }
+}
+
+public static class StringExt
+{
+    public static string Truncate(this string value, int maxLength)
+    {
+        if (string.IsNullOrEmpty(value)) return value;
+        return value.Length <= maxLength ? value : value.Substring(0, maxLength);
     }
 }
--- Assets/Scripts/IA_Item.cs
+++ Assets/Scripts/IA_Item.cs
@@ -1,24 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class IA_Item : MonoBehaviour {
+
+public class IA_Item : MonoBehaviour
+{
 
     public string currentIAItem = "none";
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    public int money;
+    private NavMeshAgent agent;
+    [SerializeField]
+    private float bananaEffect = 3;
+    [SerializeField]
+    private Transform backSpawn;
+    [SerializeField]
+
+    private Vector3 backSpawnVector;
+
+    void Start()
     {
-		
-	}
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        backSpawnVector = backSpawn.transform.position;
+        bananaEffect -= Time.deltaTime;
+        if (bananaEffect < 0)
+        {
+            Debug.Log("NoSlowed");
+            agent.speed = 10;
+            agent.acceleration = 40;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("Item");
+            UseItem();
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        //Is it the IA who enters the collider?
         if (other.tag == "MysteryBox")
         {
             Destroy(other.gameObject);
@@ -26,6 +50,28 @@ public class IA_Item : MonoBehaviour {
             if (currentIAItem == "none")
             {
                 GetRandomItem();
+
+            }
+        }
+
+        if (other.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+
+            money++;
+
+        }
+
+        if (other.tag == "Banana")
+        {
+            Destroy(other.gameObject);
+            bananaEffect = 3;
+
+            if (bananaEffect > 0)
+            {
+                Debug.Log("Slowed");
+                agent.speed = 2;
+                agent.acceleration = 8;
 
             }
         }
@@ -55,4 +101,23 @@ public class IA_Item : MonoBehaviour {
         }
     }
 
+    void UseItem()
+    {
+        if (currentIAItem == "none")
+        {
+
+        }
+        if (currentIAItem == "rocket")
+        {
+
+        }
+        if (currentIAItem == "turbo")
+        {
+
+        }
+        if (currentIAItem == "banana")
+        {
+            Instantiate(Resources.Load("Banana"), backSpawnVector, Quaternion.identity);
+        }
+    }
 }

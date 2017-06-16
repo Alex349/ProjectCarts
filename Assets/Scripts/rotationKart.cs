f136dcf9466c35853dcea7ab4ded75642c8027d0
JofreMontull <jofremontull@gmail.com>
2017-06-16 09:09:59 +0200

    Commit gold

--- Assets/Scripts/rotationKart.cs
+++ Assets/Scripts/rotationKart.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class rotationKart : MonoBehaviour {
+public class rotationKart : MonoBehaviour
+{
 
     public Transform raycastTarget;
     public Terrain terrain;
-    public Terrain CP1, CP2, CP3, CP4, CP5, CP6, CP7, CP8, CP8_1, CP9, CP10, CP11;
+    //public Terrain CP1, CP2, CP3, CP4, CP5, CP6, CP7, CP8, CP8_1, CP9, CP10, CP11;
     private NavMeshAgent agent;
     private Quaternion lookRotation;
     public float deltaRotation;
@@ -17,21 +18,20 @@ public class rotationKart : MonoBehaviour {
     {
         agent = GetComponentInParent<NavMeshAgent>();  //< cache NavMeshAgent component
         agent.updateRotation = false;          //< let us control the rotation explicitly
-        lookRotation = new Quaternion (0,0,0,0);     //< set original rotation
+        lookRotation = new Quaternion(0, 0, 0, 0);     //< set original rotation
     }
 
-    Vector3 GetTerrainNormal()
-    {
-        Vector3 terrainLocalPos = transform.position - terrain.transform.position;
-        Vector2 normalizedPos = new Vector2(terrainLocalPos.x / terrain.terrainData.size.x,
-                                            terrainLocalPos.z / terrain.terrainData.size.z);
-
-        return terrain.terrainData.GetInterpolatedNormal(normalizedPos.x, normalizedPos.y);
-    }
+    //Vector3 GetTerrainNormal()
+    //{
+    //    Vector3 terrainLocalPos = transform.position - terrain.transform.position;
+    //    Vector2 normalizedPos = new Vector2(terrainLocalPos.x / terrain.terrainData.size.x,
+    //                                        terrainLocalPos.z / terrain.terrainData.size.z);
+    //
+    //    return terrain.terrainData.GetInterpolatedNormal(normalizedPos.x, normalizedPos.y);
+    //}
     void Update()
     {
-        RaycastHit hit;
-        Vector3 normal = GetTerrainNormal();
+        RaycastHit hit;        
         Vector3 direction = agent.steeringTarget - transform.position;
         direction.y = 0.0f;
 
@@ -40,113 +40,20 @@ public class rotationKart : MonoBehaviour {
             if (hit.distance > 0.9f)
             {
                 Quaternion qLook = Quaternion.LookRotation(direction, Vector3.up);
-                Quaternion qNorm = Quaternion.FromToRotation(Vector3.up, normal);
+                Quaternion qNorm = Quaternion.FromToRotation(Vector3.up, hit.normal);
                 lookRotation = qNorm * qLook;
                 transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * deltaRotation);
             }
             else if (hit.distance <= 0.9f)
             {
                 Quaternion qLook = Quaternion.LookRotation(direction, Vector3.up);
-                Quaternion qNorm = Quaternion.FromToRotation(Vector3.up, normal);
+                Quaternion qNorm = Quaternion.FromToRotation(Vector3.up, hit.normal);
                 lookRotation = qNorm * qLook;
                 transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * normalDeltaRotation);
             }
         }
-        if (normal.magnitude > 10f)
-        {
-            //Quaternion qLook = Quaternion.LookRotation(direction, Vector3.up);
-            //Quaternion qNorm = Quaternion.FromToRotation(Vector3.up, normal);
-            //lookRotation = qNorm * qLook ;
-            //transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * deltaRotation);
-            //Debug.Log("está pujant");
-        }
-        else
-        {
-            //Quaternion qLook = Quaternion.LookRotation(direction, Vector3.up);
-            //Quaternion qNorm = Quaternion.FromToRotation(Vector3.up, normal);
-            //lookRotation = qNorm * qLook;
-            //transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * normalDeltaRotation);
-            //Debug.Log("está en pla");
-        }
-        //soften the orientation
-
     }
-
-    void OnTriggerStay (Collider theTerrainCol)
-    {
-        if (theTerrainCol.gameObject.name == "CP1")
-        {
-            terrain = CP1;
-        }
-        if (theTerrainCol.gameObject.name == "CP2")
-        {
-            terrain = CP2;
-        }
-        if (theTerrainCol.gameObject.name == "CP3")
-        {
-            terrain = CP3;
-        }
-        if (theTerrainCol.gameObject.name == "CP4")
-        {
-            terrain = CP4;
-        }
-        if (theTerrainCol.gameObject.name == "CP5")
-        {
-            terrain = CP5;
-        }
-        if (theTerrainCol.gameObject.name == "CP6")
-        {
-            terrain = CP6;
-        }
-        if (theTerrainCol.gameObject.name == "CP7")
-        {
-            terrain = CP7;
-        }
-        if (theTerrainCol.gameObject.name == "CP8")
-        {
-            terrain = CP8;
-        }
-        if (theTerrainCol.gameObject.name == "CP8.1")
-        {
-            terrain = CP8_1;
-        }
-        if (theTerrainCol.gameObject.name == "CP9")
-        {
-            terrain = CP9;
-        }
-        if (theTerrainCol.gameObject.name == "CP10")
-        {
-            terrain = CP10;
-        }
-        if (theTerrainCol.gameObject.name == "CP11")
-        {
-            terrain = CP11;
-        }
-
-    }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
